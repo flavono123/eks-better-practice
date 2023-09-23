18ca1792b93c9b365109f780f812de16ae2b376c
flavono123 <flavono123@gmail.com>
2023-09-23 18:47:40 +0900

    feat: add eks with fargate profile
    coredns on fargate profile referenece:
    https://github.com/terraform-aws-modules/terraform-aws-eks/blob/master/examples/fargate_profile/main.tf#L36-L40
    
    do in console first:
    - https://docs.aws.amazon.com/ko_kr/eks/latest/userguide/fargate-getting-started.html
    - https://docs.aws.amazon.com/ko_kr/eks/latest/userguide/pod-execution-role.html#create-pod-execution-role
    
    Signed-off-by: flavono123 <flavono123@gmail.com>

--- main.tf
+++ main.tf
@@ -12,28 +12,84 @@ terraform {
 data "aws_availability_zones" "available" {}
 
 locals {
+  vpc_name        = "ebp-vpc"
   azs             = slice(data.aws_availability_zones.available.names, 0, 2)
   vpc_cidr        = "10.0.0.0/16"
   partition       = cidrsubnets(local.vpc_cidr, 1, 1)
   private_subnets = cidrsubnets(local.partition[0], 2, 2)
   public_subnets  = cidrsubnets(local.partition[1], 2, 2)
+
+  cluster_name    = "ebp-eks"
+  cluster_version = "1.27"
 }
 
 module "ebp_vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.1.2"
 
-  name = "ebp-vpc"
+  name = local.vpc_name
   cidr = local.vpc_cidr
 
   azs             = local.azs
   private_subnets = local.private_subnets
   public_subnets  = local.public_subnets
 
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
   enable_nat_gateway = true
   single_nat_gateway = true
 
   tags = {
-    Name = "ebp-vpc"
+    Name = local.vpc_name
+  }
+}
+
+module "ebp_eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.16.0"
+
+  cluster_name    = local.cluster_name
+  cluster_version = local.cluster_version
+
+  vpc_id     = module.ebp_vpc.vpc_id
+  subnet_ids = module.ebp_vpc.private_subnets
+
+  cluster_endpoint_public_access = true
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+      configuration_values = jsonencode({
+        computeType = "Fargate"
+      })
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+    }
+  }
+
+  fargate_profiles = {
+    default = {
+      name = "default"
+      selectors = [
+        {
+          namespace = "kube-system"
+        },
+        {
+          namespace = "default"
+        }
+      ]
+    }
+  }
+
+  tags = {
+    Name = local.cluster_name
   }
 }
